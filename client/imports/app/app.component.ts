88cad2c305c4835f9b099510290a0be0a8606e26
Haden Hiles <hadenhiles@gmail.com>
2017-09-10 14:15:23 -0400

    add custom material theme

--- client/imports/app/app.component.ts
+++ client/imports/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from "@angular/core";
 import template from "./app.component.html";
 import style from "./app.component.scss";
+import theme from "./app.theme.scss";
 
 @Component({
   selector: "app",
   template,
-  styles: [ style ]
+  styles: [ style, theme ]
 })
 export class AppComponent {
   constructor() {
